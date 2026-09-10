9c9aa74c218593689679643421948a9d9a5cb00b
MisterMallard <calvie.bo.balvie@gmail.com>
2026-09-10 13:15:15 +0200

    death to the github devs

---
+++ Opdracten/1.1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Opdracten
+{
+	internal class _1
+	{
+		static void Main(string[] args)
+		{
+			string naam = "Erwin";
+			int score = 1000;
+			bool alive = true;
+
+			Console.WriteLine("Naam : " + naam);
+			Console.WriteLine("Score : " + score);
+			Console.WriteLine("Alive : " + alive);
+		}
+	}
+}
